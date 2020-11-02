7f5028566fff54a1c89217d69119b594f63ecbcb
Matt Scilipoti <matt@scilipoti.name>
2020-11-02 09:55:33 -0500

    Add/configure CableReady Timeline
    - $ bin/rails g channel Timeline
    - identify user by session

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # isolate users by session, as they connect on websocket
+    identified_by :session_id
+
+    def connect
+      self.session_id = request.session.id
+    end
   end
 end
