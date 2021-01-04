1f7ba343b3350367603c0c72aa30f61155e7702d
Matt Scilipoti <matt@scilipoti.name>
2021-01-04 17:17:56 -0500

    Config: Stimulus Reflex requires caching in dev

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,6 +3,13 @@
 # start redis, without persistence, as background process
 redis-server  --save "" --appendonly no &
 
+# Stimulus Reflex requires caching to be enabled. Caching allows the session to be modified during ActionCable requests.
+cache_dev_config_file=./tmp/caching-dev.txt
+# -r: Return true value, if file exists and is readable
+if test ! -r "$cache_dev_config_file"; then
+  bundle exec rails dev:cache
+fi
+
 # start rails server,
 # - use binding 0.0.0.0 for visibility from host browser
 bundle exec rails server --binding 0.0.0.0
